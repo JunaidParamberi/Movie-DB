4b7a4f00644911c71839064ba146122c4ced842b
junaidparamberi <junaidkdr313@gmail.com>
2023-12-26 23:29:08 +0400

    new ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,52 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import Header from "./Components/Header";
-import MovieCard from "./Components/MovieCard";
-import Filter from "./Components/Filter";
-
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import TvShowCard from "./pages/TvShowCard";
+import AboutUs from "./pages/AboutUs";
+import Hero from "./components/Hero";
+import MovieCard from "./pages/MovieCard";
+import MovieDetails from "./pages/MovieDetails";
+import TvShowDetails from "./pages/TvShowDetails";
 function App() {
-  const API_KEY = "f2db34f2c291500db37a87ab83ea5545";
-  const BASE_URL = "https://api.themoviedb.org/3/search/movie";
-
-  const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [defaultMovie, setDefaultMovies] = useState([]);
-
-  useEffect(() => {
-    const defaultMovies = () => {
-      const defaultAPi =
-        "https://api.themoviedb.org/3/discover/movie?api_key=f2db34f2c291500db37a87ab83ea5545";
-
-      fetch(defaultAPi)
-        .then((res) => res.json())
-        .then((data) => {
-          setDefaultMovies(data.results);
-        })
-        .catch((error) => {
-          console.error("Error fetching default movies:", error);
-        });
-    };
-
-    defaultMovies();
-  }, []);
-
-  useEffect(() => {
-    fetch(`${BASE_URL}?api_key=${API_KEY}&query=${searchTerm}`)
-      .then((response) => response.json())
-      .then((data) => {
-        if (Array.isArray(data.results)) {
-          setMovies(data.results);
-        } else {
-          console.error("Invalid data format:", data);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching movies:", error);
-      });
-  }, [searchTerm]);
-
-  const displayMovies = movies.length > 0 ? movies : defaultMovie;
-
-  const moveCardElements = displayMovies.map((movie) => (
-    <MovieCard key={movie.id} movie={movie} />
-  ));
+  const [searchInput, setSearchInput] = useState("");
 
   return (
-    <div className="App">
-      <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <Filter />
-      <div className="card">{moveCardElements}</div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            path="/"
+            element={
+              <Hero searchInput={searchInput} setSearchInput={setSearchInput} />
+            }
+          >
+            <Route
+              index
+              element={
+                <MovieCard
+                  searchInput={searchInput}
+                  setSearchInput={setSearchInput}
+                />
+              }
+            />
+            <Route
+              path="tvshow"
+              element={
+                <TvShowCard
+                  searchInput={searchInput}
+                  setSearchInput={setSearchInput}
+                />
+              }
+            />
+          </Route>
+
+          <Route path="/:id" element={<MovieDetails />} />
+          <Route path="tvshow/:id" element={<TvShowDetails />} />
+          <Route path="aboutus" element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,42 +1,40 @@
 import React from "react";
-import searchIcon from "../assets/Search.svg";
+import Logo from "../assets/Logo.svg";
+import { NavLink, Link } from "react-router-dom";
 
-const Header = ({ searchTerm = "", setSearchTerm }) => {
-  const searchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearch = (event) => {
-    event.preventDefault();
-    const newSearchTerm = event.target.elements.search.value;
-    setSearchTerm(newSearchTerm);
+function Header() {
+  const activeStyle = {
+    color: "white",
   };
 
   return (
-    <div className="navbar">
-      <div className="logo-and-input">
-        <h1>Movies</h1>
-        <form onSubmit={handleSearch} action="">
-          <img src={searchIcon} alt="search icon" />
-          <input
-            type="search"
-            placeholder="Search for movies and more..."
-            value={searchTerm}
-            onChange={searchChange}
-            name="search"
-          />
-          <button>Search</button>
-        </form>
-      </div>
+    <div className="header-container">
+      <Link to="/">
+        <img src={Logo} alt="Movie db logo" />
+      </Link>
 
-      <div className="nav-links">
-        <ul>
-          <li>Login</li>
-          <li>Register</li>
-        </ul>
+      <div className="nav-link">
+        <NavLink
+          to="/"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          Movies
+        </NavLink>
+        <NavLink
+          to="tvshow"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          TV Show
+        </NavLink>
+        <NavLink
+          to="aboutus"
+          style={({ isActive }) => (isActive ? activeStyle : null)}
+        >
+          About Us
+        </NavLink>
       </div>
     </div>
   );
-};
+}
 
 export default Header;
